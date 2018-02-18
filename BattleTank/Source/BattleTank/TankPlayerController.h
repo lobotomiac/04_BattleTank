fadea1d6a0c800bb278a9069b367e90569f8f0ee
Marko <marko.simunovic89@gmail.com>
2018-02-18 22:09:07 +0100

    L15 Delegating to Components/Fixed
    After for some reason numerous issues and random unexpected renaming instances, fixed class names, includes and engine - VS connection

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -3,18 +3,20 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Tank.h"
 #include "GameFramework/PlayerController.h"
-#include "TankPlayerComponent.generated.h"
+#include "TankPlayerController.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class BATTLETANK_API ATankPlayerComponent : public APlayerController
+class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-	
+public: 
+	ATank * GetControlledTank() const;
 	
 	
 };
