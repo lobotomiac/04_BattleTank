32721a59625248f97c8da73d2bd9d53a2dcd45f9
Marko <marko.simunovic89@gmail.com>
2018-02-19 21:04:20 +0100

    Using virtual and override
    Grasping the concept of overriding virtual methods / cleaning up inventory

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -1,12 +1,29 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#
+
 #include "TankPlayerController.h"
 
+
+ void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("TankPlayerController BeginPlay Log"))
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("PLayerController does not possess a tank."))
+		return;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s possesses a tank"),*GetName())
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
-
 
